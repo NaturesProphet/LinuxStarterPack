cf471bf64235ca0ff4a8cfc2ec72e13b352c09d7
Mateus Garcia <mateus.gigainfo@gmail.com>
2020-03-30 10:19:03 -0300

    Update SCRIPT.sh

--- SCRIPT.sh
+++ SCRIPT.sh
@@ -63,7 +63,7 @@ echo 'fs.inotify.max_user_watches=524288' >> /etc/sysctl.conf
 cd /$usuario/Downloads
 curl get.docker.com -Lo - | sh
 usermod -aG docker $usuario
-curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.25.4/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 #####################################################################
